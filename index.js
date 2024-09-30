ea32d89285cdf1332747fa9454cabe62903490ce
lawrencek <spiceeast509@gmail.com>
2024-09-30 14:26:49 +0300

    array-f-method

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(array){
+    const win = array.find(point=> point.result ==="W")
+return win ? win.year: undefined;
+}
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+const result = superbowlWin(record)
+console.log(result)
